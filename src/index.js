50638b3c63b0240aab5bca4a1c8834c61a8573c6
JungHak <lovebox0310@gemail.com>
2022-02-27 23:34:01 +0900

    App5.js 컴포넌트 형식 구조 추가.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App4 from './App4'
+import App5 from './App5'
 
 ReactDOM.render(
 
   <React.StrictMode>
-    <App4 />
+    <App5 />
   </React.StrictMode>,
   document.getElementById('root')
 )
