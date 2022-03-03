97dd3c242909149da5ff5f4a710a4d0c68133053
jh.kim <lovebox0310@gmail.com>
2022-03-03 14:38:20 +0900

    충돌 머지처리함.

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-import React from "react";
-import { Children } from "react/cjs/react.production.min";
-// import { Value } from "sass";
-import styled from "styled-components";
-=======
 import React from 'react'
 import styled from 'styled-components'
->>>>>>> 509cd0d7eecc32511ab0ba5078f314f10484d054
 
 const Btn = styled.button`
   width: 70px;
@@ -18,17 +11,6 @@ const Btn = styled.button`
   box-sizing: border-box;
 `
 
-<<<<<<< HEAD
-// const Button = ({ children, handleDeleteBtn, itemId }) => {
-const Button = ({props, children, handleDeleteBtn,  }) => {
-  // console.log('>> handleDeleteBtn', handleDeleteBtn);
-  // console.log('>> children', children);
-  // console.log(props);
-  // return null;
-  return <Btn onClick={(e) => handleDeleteBtn && handleDeleteBtn(props.itemId)}>{children}</Btn>;
-  // return <Btn onClick={(e) => props.handleDeleteBtn && props.handleDeleteBtn(props.itemId)}>{props.namevalue}</Btn>;
-};
-=======
 const Button = ({ children, handleDeleteBtn, itemId }) => {
   return (
     <Btn onClick={(e) => handleDeleteBtn && handleDeleteBtn(itemId)}>
@@ -36,6 +18,5 @@ const Button = ({ children, handleDeleteBtn, itemId }) => {
     </Btn>
   )
 }
->>>>>>> 509cd0d7eecc32511ab0ba5078f314f10484d054
 
 export default Button
