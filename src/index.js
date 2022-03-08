220d41a566d23b20e8431b7513fb8d8fb2d17fa5
sub <spoon718@gmail.com>
2022-03-08 14:30:21 +0900

    merge

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App5 from './App5';
-=======
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App6 from './App6'
->>>>>>> 97dd3c242909149da5ff5f4a710a4d0c68133053
 
 ReactDOM.render(
 
   <React.StrictMode>
-<<<<<<< HEAD
-    <App5 />
-=======
     <App6 />
->>>>>>> 97dd3c242909149da5ff5f4a710a4d0c68133053
   </React.StrictMode>,
   document.getElementById('root')
 )
